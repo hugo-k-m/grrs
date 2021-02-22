10faeb4dfdda9f89ce98257401e5ae4b7a8507cd
Hugo <57953645+hugo-k-m@users.noreply.github.com>
2021-02-22 11:09:28 +0200

    Error format
    - Implemented a custom error for when the file or directory isn't found.

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,4 +1,16 @@
+use std::error::Error;
 use structopt::StructOpt;
+
+#[derive(Debug)]
+struct CustomError(String);
+
+impl std::fmt::Display for CustomError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        f.write_str("Custom error.")
+    }
+}
+
+impl Error for CustomError {}
 
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(StructOpt)]
@@ -12,7 +24,8 @@ struct Cli {
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Cli::from_args();
-    let content = std::fs::read_to_string(&args.path)?;
+    let content = std::fs::read_to_string(&args.path)
+        .map_err(|err| CustomError(format!("Error reading `{}`: {}", &args.path.display(), err)))?;
 
     for line in content.lines() {
         if line.contains(&args.pattern) {
