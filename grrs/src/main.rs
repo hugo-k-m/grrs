83c0816c08af1166594d1b038d7d8c7b52887e67
Hugo <57953645+hugo-k-m@users.noreply.github.com>
2021-03-15 11:17:56 +0200

    Added '--verbose' flag to project

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -5,15 +5,20 @@ use std::{
     io::{stdout, BufReader},
 };
 use structopt::StructOpt;
+use clap_verbosity_flag::Verbosity;
 
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(StructOpt)]
 struct Cli {
     /// The pattern to look for.
     pattern: String,
+
     /// The path to the file to read.
     #[structopt(parse(from_os_str))]
     path: std::path::PathBuf,
+
+    #[structopt(flatten)]
+    verbose: Verbosity
 }
 
 fn main() -> Result<()> {
