eb34b7d4ea6cc2984772e82d2a05f90a2f9c52b8
Hugo <57953645+hugo-k-m@users.noreply.github.com>
2021-03-15 10:36:28 +0200

    perf: BufReader
    Replace read_to_string() with BufReader for performance.

--- grrs/src/lib.rs
+++ grrs/src/lib.rs
@@ -1,12 +1,35 @@
 use anyhow::Result;
-use std::io::Write;
+use std::{
+    fs::File,
+    io::{BufRead, BufReader, Write},
+};
 
-pub fn find_matches(content: String, pattern: &str, mut writer: impl Write) -> Result<()> {
-    for line in content.lines() {
+pub fn find_matches(content: BufReader<File>, pattern: &str, mut writer: impl Write) -> Result<()> {
+    for content_line in content.lines() {
+        let line = content_line?;
+
         if line.contains(pattern) {
             writeln!(writer, "{}", line)?;
         }
     }
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use anyhow::Result;
+    use std::io::BufReader;
+    #[test]
+    fn find_match() -> Result<()> {
+        let mut result = Vec::new();
+        let file = File::open("unit_test.txt")?;
+        let reader = BufReader::new(file);
+
+        find_matches(reader, "lorem", &mut result)?;
+        assert_eq!(&result, "lorem ipsum\n".as_bytes());
+
+        Ok(())
+    }
 }
--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,6 +1,9 @@
 use anyhow::{Context, Result};
 use log::info;
-use std::io::stdout;
+use std::{
+    fs::File,
+    io::{stdout, BufReader},
+};
 use structopt::StructOpt;
 
 /// Search for a pattern in a file and display the lines that contain it.
@@ -16,29 +19,19 @@ struct Cli {
 fn main() -> Result<()> {
     env_logger::init();
 
-    info!("starting up");
+    info!("reading file");
 
     let args = Cli::from_args();
     let path = &args.path;
-    let content = std::fs::read_to_string(path)
-        .with_context(|| format!("Could not read file `{}`", path.display()))?;
+
+    let f =
+        File::open(path).with_context(|| format!("Could not read file `{}`", path.display()))?;
+
+    let reader = BufReader::new(f);
 
     info!("finding matches");
 
-    grrs::find_matches(content, &args.pattern, &mut stdout().lock())?;
-
-    Ok(())
-}
-
-#[test]
-fn find_match() -> Result<()> {
-    let mut result = Vec::new();
-    grrs::find_matches(
-        "lorem ipsum\ndolor sit amet".to_string(),
-        "lorem",
-        &mut result,
-    )?;
-    assert_eq!(result, b"lorem ipsum\n");
+    grrs::find_matches(reader, &args.pattern, &mut stdout().lock())?;
 
     Ok(())
 }
